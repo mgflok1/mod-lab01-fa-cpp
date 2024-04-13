811e00ac014d11f987224e19c0213ecc9ec55228
mgflok1 <134943906+mgflok1@users.noreply.github.com>
2024-04-13 18:02:36 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -2,6 +2,7 @@
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
 
+bool containsDigit(const char *word);
 unsigned int faStr1(const char *str);
 unsigned int faStr2(const char *str);
 unsigned int faStr3(const char *str);
