a18cd3a9d8f591f3745139d38711197f269e87f5
mgflok1 <134943906+mgflok1@users.noreply.github.com>
2024-04-13 18:13:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
+    const char *str = "hello Test string123 For Lab1 ";
+
+    std::cout << "\nКоличество слов, не содержащих цифр: " << faStr1(str);
+    std::cout << "\nКоличество слов, начинающиеся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы: " << faStr2(str);
+    std::cout << "\nСредняя длина слов: " << faStr3(str);
+
     return 0;
 }
